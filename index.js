bd81927a0a84c0064f123a7526aee40bbadfd900
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-10-12 18:31:12 +0530

    delete functionality

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ import fs from "fs";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-let dbData;
+let dbData = [];
 fs.readFile("./db.json", "utf-8", (err, jsonString) => {
   if (err) console.log(err);
   else dbData = JSON.parse(jsonString);
-  //   console.log("jsonString", JSON.parse(jsonString).users);
 });
 app.get("/", (req, res) => {
   res.render("index.ejs", {
     // content: data.users,
     content: dbData,
   });
-  // res.render("NewUser.ejs")
 });
 app.get("/addNew", (req, res) => {
   res.render("NewUser.ejs");
@@ -24,13 +22,33 @@ app.get("/addNew", (req, res) => {
 app.post("/create", (req, res) => {
   let newData = { ...req.body, id: dbData.length + 1 };
   dbData.push(newData);
-  //   console.log("dbData",dbData);
-  fs.writeFile("./db.json", JSON.stringify(dbData,null, 2), (err) => {
+  fs.writeFile("./db.json", JSON.stringify(dbData, null, 2), (err) => {
     // Error checking
     if (err) throw err;
     console.log("New data added");
     res.render("success.ejs", { message: "User Added successfully!" });
   });
+});
+app.get("/delete/:id", (req, res) => {
+  let filteredIndex = dbData.findIndex((obj) => obj.id == req.params.id);
+  dbData.splice(filteredIndex, 1);
+  fs.writeFile("./db.json", JSON.stringify(dbData, null, 2), (err) => {
+    // Error checking
+    if (err) throw err;
+    console.log("New data added");
+    res.render("success.ejs", { message: "User Deleted successfully!" });
+  });
+});
+app.get("/edit/:id", (req, res) => {
+  let filteredObj = dbData.find((obj) => obj.id == req.params.id);
+  res.render("updateUser.ejs", { editing: true, content: filteredObj });
+  // dbData.splice(filteredIndex, 1);
+  // fs.writeFile("./db.json", JSON.stringify(dbData, null, 2), (err) => {
+  //   // Error checking
+  //   if (err) throw err;
+  //   console.log("New data added");
+  //   res.render("success.ejs", { message: "Details edited successfully!" });
+  // });
 });
 app.listen(3000, () => {
   console.log("server is running on port 3000");
