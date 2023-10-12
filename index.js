9850d776a160fbea4ad2a9a0a73eac6e7b664282
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-10-12 15:05:55 +0530

    all users display UI implementation

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 import bodyParser from "body-parser";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+import data from "./db.json" assert  { type: "json" };
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    content: data.users,
+  });
+});
+app.post("/create", (req, res) => {
+  console.log(req.body);
 });
 app.listen(3000, () => {
   console.log("server is running on port 3000");
