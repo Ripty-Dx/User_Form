1eb38b691d9edaabe23524f74f3762a40519abb9
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-10-13 12:39:09 +0530

    Edit functionality

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/addNew", (req, res) => {
   res.render("NewUser.ejs");
 });
 app.post("/create", (req, res) => {
-  let newData = { ...req.body, id: dbData.length + 1 };
+  let newData = { ...req.body, id: Number(dbData[dbData.length - 1].id) + 1 };
   dbData.push(newData);
   fs.writeFile("./db.json", JSON.stringify(dbData, null, 2), (err) => {
     // Error checking
@@ -41,7 +41,8 @@ app.get("/delete/:id", (req, res) => {
 });
 app.get("/edit/:id", (req, res) => {
   let filteredObj = dbData.find((obj) => obj.id == req.params.id);
-  res.render("updateUser.ejs", { editing: true, content: filteredObj });
+  res.render("updateUser.ejs", { content: filteredObj });
+
   // dbData.splice(filteredIndex, 1);
   // fs.writeFile("./db.json", JSON.stringify(dbData, null, 2), (err) => {
   //   // Error checking
@@ -49,6 +50,18 @@ app.get("/edit/:id", (req, res) => {
   //   console.log("New data added");
   //   res.render("success.ejs", { message: "Details edited successfully!" });
   // });
+});
+app.post("/updated/:id", (req, res) => {
+  console.log("updated");
+  console.log(req.body, req.params);
+  let filteredIndex = dbData.findIndex((obj) => obj.id == req.params.id);
+  let newData = { ...req.body, id: req.params.id };
+  dbData[filteredIndex] = newData;
+  fs.writeFile("./db.json", JSON.stringify(dbData, null, 2), (err) => {
+    // Error checking
+    if (err) throw err;
+    res.render("success.ejs", { message: "Details updated successfully!" });
+  });
 });
 app.listen(3000, () => {
   console.log("server is running on port 3000");
